21e388a5106af68852e24e2687b3621eaefafe4b
shelihaeva <smelkovagolodnova@yandex.ru>
2023-12-16 18:15:25 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# введите два сила и определите большее из них
+a = int (input ("Введите первое число "))
+b = int (input("Введите число "))
+ #b = 2
+ #c = a % b
+ #print (type[c],c)
+if a > b:
+ # print(a,"bigger", b, sep= '++', end= '------')
+ print(f'{a},2bigger, {b}')
+ else:
+    print
+ 
+# != не равно
+# == равно
+# #введите два числа и определите большее из них
+a = int(input("введите первое число "))
+b = int(input("введите  число " ))
+
+if :
+    print(f'{a} 1bigger {b}')
+if a == b:
+    print(f'{a} same {b}')
+if a < b:
+    print(f'{b} 2bigger {a}')
+
+if a > b:
+    print(f'{a} 1bigger {b}')
+elif a == b:
+    print(f'{a} same {b}')
+elif a < b:
+    print(f'{b} 2bigger {a}')
+# print(type(c),c)
