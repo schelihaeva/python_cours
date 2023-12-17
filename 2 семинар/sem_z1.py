5068a913ede195566fdbe915f2293be7286df430
shelihaeva <smelkovagolodnova@yandex.ru>
2023-12-17 11:30:29 +0300

    работа на 2м семинаре

---
+++ 2 семинар/sem_z1.py
@@ -0,0 +1,13 @@
+#str1 ='abcdef'
+#print(str1[1:5:2],str1[4],str1[-1])
+
+#str2 ='123654'
+#print(int(str2[0])+int[str2])
+
+#While с помощью этого цикло посчитать все числа от 0 до х
+х = 1000
+sum1 = 0
+while x > 0:
+    sum1 = sum1 + x
+    x = x-1
+print(sum1)    
