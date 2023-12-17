0e8b00586c4cd7ae7dafd46f90c252bbd0626d78
shelihaeva <smelkovagolodnova@yandex.ru>
2023-12-17 12:00:31 +0300

    работа на 2м семинаре

---
+++ 2 семинар/sem_z4.py
@@ -0,0 +1,14 @@
+size = 5
+num = int(input('num= '))
+max1 = num1
+min1=num1
+
+while size > 0:
+      size-=1
+      
+    
+      print(max1)
+      if max1< num1:
+        max1 = num1
+      print(max1)
+print(max1)    
---
+++ 2 семинар/sem_z5.py
@@ -0,0 +1,12 @@
+x + y = 5
+y = 5 - x
+x * y = 6
+
+for x in range(5+1):
+    if x * (5-x) == 6:
+        print(y = 5 - x, x)
+
+for x in range(5+1):
+    for y in range(5+1):
+        if  x + y ==5 and x*y ==6:
+            print(x,y)
