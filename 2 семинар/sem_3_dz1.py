551013a0e751b71eb101fb27b0b41242c7f805f9
shelihaeva <smelkovagolodnova@yandex.ru>
2023-12-23 20:27:57 +0300

    Решение задачи на семинаре

---
+++ 2 семинар/sem_3_dz1.py
@@ -0,0 +1,34 @@
+# Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+
+# Найдите количество и выведите его.
+# list_1 = [1, 2, 3, 4, 5, 3, 3] 
+# print(list_1)
+# k= int(input("Введите элемент из списка"))
+# conunt = list_1.count('Количество элементов в списке: ')
+# print(conunt)
+
+# 2 cпособ
+# list_1 = [1, 2, 3, 2, 4, 1, 5, 2]
+# k = [2]
+# for item in list_1:
+#     if list_1.count(item) > 1 and item not in k:
+#         k.append(item)
+
+# print("Повторяющиеся элементы в списке:", k)
+
+# list_1 = [1, 2, 3, 2, 4, 1, 5, 2, 1]
+# k = count(list_1)
+# for item, count in k.items():
+#     if count > 1:
+#         print(f"Элемент {item} повторяется {count} раз(а).")
+
+        # 
+list_1 = [1, 2, 3, 4, 5, 3, 3]    
+k = 5  #вводим число из списка
+count_k = 0
+
+for num in list_1: # задаем цикл
+    if num == k: # условие если num==k
+        count_k += 1 #если условие не выполнено, то увеличиваем на 1
+
+print(count_k)
