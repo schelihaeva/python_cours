765ecaa1d2636bcaa661f3375a0ce4a15c059b30
shelihaeva <smelkovagolodnova@yandex.ru>
2023-12-24 11:24:32 +0300

    решение задач на 4м семинаре

--- семинары Phython/sem_4_z25.py
+++ семинары Phython/sem_4_z25.py
@@ -4,7 +4,39 @@
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 # Для решения данной задачи используйте функцию .split()
 
-lst = 'a a a b c a a d c d d'
-print(lst)
-lst = 'a a a b c + + a       a +d +c       d d'.replace('+'," ").split()
-print(lst)
+# lst = 'a a a b c a a d c d d'
+# print(lst)
+# lst = 'a a a b c + + a       a +d +c       d d'.replace('+'," ").split()
+# print(lst)
+# str = 'a a a b c a a d c d d'
+# list_1 = str.split()
+# print(str)
+# result = list()
+
+# for i in list_1:
+#     count = 0
+#     if i in result:
+#         for j in result:
+#             if i == j[0]:
+#                 count += 1
+#         result.append(f"{i}_{count}")
+#     else:
+#         result.append(i)
+
+# print(" ".join(result))
+str = 'a a a b c a a d c d d'
+list_1 = str.split()
+print(str)
+result = list()
+
+for i in list_1:
+    count = 0
+    if i in result:
+        for j in result:
+            if i == j[0]:
+                count += 1
+        result.append(f"{i}_{count}")
+    else:
+        result.append(i)
+
+print(" ".join(result))
---
+++ семинары Phython/sem_4_z27.py
@@ -0,0 +1,10 @@
+# Задача No27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells
+# Output: 13
+# 15 минут
+input = "She sells sea shells on the sea shore The shells that she sells are sea "\
+    "shells I'm sure So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+    # lst - создание списка, при помощи методов lower и split
+lst = input.lower().split(' ') # Метод lower() — это строковый метод, который возвращает новую строку полностью в нижнем регистре.Метод split()разбивает строку на слова, используя в качестве разделителя последовательность пробельных символов.
+print(len(set(lst)))
