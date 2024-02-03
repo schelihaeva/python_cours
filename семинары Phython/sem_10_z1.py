2b78856385a8a365a80fd7f3f6ef0056e63e99c6
shelihaeva <smelkovagolodnova@yandex.ru>
2024-02-03 11:51:33 +0300

    sem_10

---
+++ семинары Phython/sem_10_z1.py
@@ -0,0 +1,28 @@
+# import random
+# lst = ['robot'] * 10
+# lst += ['human'] * 10
+# random.shuffle(lst)
+# data = pd.DataFrame({'whoAmI':lst, '42':[i for i in lst] })
+# data['42'] = data['42'].map({'robot' : 666})
+# data = data.drop('42',axis = 1)
+# data.head()
+
+import pandas as pd 
+import numpy as np 
+import random
+#Импортируем библотеки 
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+print(data)
+ 
+print('')
+
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
+#
